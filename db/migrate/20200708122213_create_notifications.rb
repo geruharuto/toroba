0ffd47f4e01fd4f0ecf6682c7a066088650f4d80
EC2 Default User <ec2-user@ip-172-31-40-138.ap-northeast-1.compute.internal>
2020-07-18 06:04:08 +0000

    テストデプロイ、マイグレーション修正

--- db/migrate/20200708122213_create_notifications.rb
+++ db/migrate/20200708122213_create_notifications.rb
@@ -1,14 +1,17 @@
 class CreateNotifications < ActiveRecord::Migration[5.2]
   def change
     create_table :notifications do |t|
-      t.references :tweet, type: :bigint, foreign_key: true
-      t.references :active, type: :bigint, foreign_key: true
-      t.references :passive, type: :bigint, foreign_key: true
-      t.references :comment, type: :bigint, foreign_key: true
+      # t.references :tweet, type: :bigint, foreign_key: true
+      t.references :active, type: :bigint, foreign_key: { to_table: :users }
+      t.references :passive, type: :bigint, foreign_key: { to_table: :users }
+      # t.references :comment, type: :bigint, foreign_key: true
       t.string :action
       t.boolean :checked, default: false, null: false
 
       t.timestamps
     end
+    # add_reference :notifications, :active, foreign_key: true
+    # add_reference :notifications, :passive, foreign_key: true
+    add_reference :notifications, :comment, foreign_key: true
   end
 end
