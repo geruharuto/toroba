1e8815e1a724bdd094df0cba4595a024ea36bd3e
geruharuto <geruharuto2121@yahoo.co.jp>
2020-08-18 19:46:17 +0900

    見た目調整

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,15 +4,13 @@ class FavoritesController < ApplicationController
   def create
     tweet = Tweet.find(params[:tweet_id])
     favorite = current_user.favorites.new(tweet_id: tweet.id)
-    favorite.save!
+    favorite.save
     tweet.create_notification_favorite!(current_user)
-    redirect_to request.referer
   end
 
   def destroy
     tweet = Tweet.find(params[:tweet_id])
     favorite = current_user.favorites.find_by(tweet_id: tweet.id)
-    favorite.destroy!
-    redirect_to request.referer
+    favorite.destroy
   end
 end
